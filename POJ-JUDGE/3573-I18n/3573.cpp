7db20973482acc3a648c2456aac15239a629b390
Esteban Foronda Sierra <seforonda@gmail.com>
2013-09-08 06:52:57 +0200

    I18n Problem

---
+++ POJ-JUDGE/3573-I18n/3573.cpp
@@ -0,0 +1,138 @@
+# include <algorithm>
+# include <iostream>
+# include <iterator>
+# include <sstream>
+# include <fstream>
+# include <cassert>
+# include <cstdlib>
+# include <string>
+# include <cstring>
+# include <cstdio>
+# include <vector>
+# include <cmath>
+# include <queue>
+# include <stack>
+# include <map>
+# include <set>
+
+using namespace std;
+map <string, string> wordtoabs,abstoword;
+
+string find(string y){
+    stringstream ss;
+    ss << y.size()-2;
+    return y[0] + ss.str() + y[y.size()-1];   
+}
+
+
+
+string convertwordtoabs(string x){    
+    if(wordtoabs[x] == ""){
+        string aux = find(x);
+        wordtoabs[x] = aux;
+        if(abstoword[aux] == ""){
+            abstoword[aux] = x;
+            return x;     
+        } else {
+            if(abstoword[aux] == x) return x;
+            else {
+                abstoword[aux] = "amb4543";
+                return x;    
+            }   
+        }
+    }
+    return x;
+  
+}
+
+string convertabstoword(string x){
+    if(abstoword[x] != "" && abstoword[x] != "amb4543")return abstoword[x];
+    if(abstoword[x] != "" && abstoword[x] == "amb4543")return x;
+    return x;
+    
+}
+
+string cmayus(string x){
+    string ans = "";
+    for(int i=0; i<x.size() ; i++){
+       ans += toupper(x[i]);
+    }  
+    return ans; 
+}
+
+int wordorabs(string word, string nword, char y){
+    if(y=='1'){
+        if((word.size() == 3 && isdigit(word[1])) || (word.size() == 4 && 
+        isdigit(word[1]) && isdigit(word[2]))){
+            string cword = convertabstoword(word);
+            if(isupper(nword[0]) && isupper(nword[nword.size()-1])){
+                string ans = cmayus(cword); 
+                cout << ans << endl;   
+            } else{
+                cout << nword[0] << cword.substr(1,cword.size()-2); 
+                cout<< nword[nword.size()-1] << endl;
+            }
+            
+                
+        } else {
+        convertwordtoabs(word);    
+        cout << nword << endl;   
+        }         
+        return 0;
+    }    
+        
+    if((word.size() == 3 && isdigit(word[1])) || (word.size() == 4 && 
+        isdigit(word[1]) && isdigit(word[2]))){
+            string cword = convertabstoword(word);
+            if(isupper(nword[0]) && isupper(nword[nword.size()-1])){
+                string ans = cmayus(cword); 
+                cout << ans << y;   
+            } else{
+                cout << nword[0] << cword.substr(1,cword.size()-2); 
+                cout<< nword[nword.size()-1] << y;
+            }
+            
+                
+    } else {
+        convertwordtoabs(word);    
+        cout << nword << y;   
+    }         
+
+return 0;   
+}     
+
+
+int main(){
+    string line;
+    while (getline(cin, line)){
+        string word, nword;
+        for(int i=0; i<line.size(); i++){
+            if(line[i] == '-'  || line[i] == ',' || line[i] == '.' 
+                || line[i] == '\"' || line[i] == '(' || line[i] == ')' 
+                || line[i] == ':'  || line[i] == ';'  || line[i] == '!' 
+                || line[i] == '?'  || line[i] == ' '){
+                
+                if(word.size()>= 3){
+                    wordorabs(word, nword, line[i]);
+                }
+                else cout << nword << line[i];
+                nword = word = "";        
+            } else {
+                nword += line[i];   
+                word += tolower(line[i]);
+            }  
+        } 
+        
+        if(word.size()>= 3){
+            char y = '1';    
+            wordorabs(word, nword, y);
+        }
+        else cout << nword << endl;
+        nword = word = "";        
+
+    }
+
+  
+
+return 0;
+}
