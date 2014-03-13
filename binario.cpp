d3d135449820a3480141a4a8d34383c2773cd6ca
unknown <eforonda@gmail.com>
2014-03-13 13:56:58 -0500

    Esto es una prueba

---
+++ binario.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+#define D(x) cout << #x " is " << x << endl
+
+using namespace std;
+int find(string x){
+    
+    
+           
+    
+}
+int main(){
+    string n;
+    while(cin>>n){
+        find(n);              
+    }
+	return 0;
+}
