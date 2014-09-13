71f34db140001375e799eb87aca2a8565c352815
EstebanFS <seforonda@gmail.com>
2014-09-12 23:42:20 -0500

    Updating ACIS REDIS 2013 Contest

--- Contests/ACIS REDIS Contests/ACIS REDIS 2013/Warm-up/Bases.cpp
+++ Contests/ACIS REDIS Contests/ACIS REDIS 2013/Warm-up/Bases.cpp
@@ -1,21 +1,32 @@
-# include <algorithm>
-# include <iostream>
-# include <iterator>
-# include <sstream>
-# include <fstream>
-# include <cassert>
-# include <cstdlib>
-# include <string>
-# include <cstring>
-# include <cstdio>
-# include <vector>
-# include <cmath>
-# include <queue>
-# include <stack>
-# include <map>
-# include <set>
-
+//Esteban Foronda Sierra
 using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+template <class T> string toStr(const T &x)
+{ stringstream s; s << x; return s.str(); }
+template <class T> int toInt(const T &x)
+{ stringstream s; s << x; int r; s >> r; return r;}
+
+#define D(x) cout << #x " is " << x << endl
 
 int b1, b2, b3, a, b;
 
